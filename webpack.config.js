8528360cd03b3c09f895f2e59e00f3777d97077d
arash <Arash Kiani>
2018-08-24 14:27:30 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,36 @@
 const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './lib/web37.js',
   output: {
     filename: 'web37.min.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
